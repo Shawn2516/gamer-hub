ebe90c8e9f412d82db54c8fe29aea0db842022b4
Shawn2516 <shawnmusick@gmail.com>
2023-10-27 01:32:46 +0300

    Added image

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -6,6 +6,9 @@ import Navigation from './components/Navigation.vue';
 <template>
   <Navigation/>
   <h2>Welcome to our Amazing Website</h2>
+  <div class="imageHolder">
+    <img src="../public/holographic_image.webp">
+  </div>
 </template>
 
 <style scoped>
@@ -13,6 +16,20 @@ import Navigation from './components/Navigation.vue';
     padding: 5px;
     color: #f1f1f1;
     text-align: center;
+  }
+
+  .imageHolder {
+    display: flex;
+    width: 300px;
+    height: 300px;
+    margin: 20px auto;
+  }
+
+  img {
+    transform: scale(100%);
+    border-radius: 50%;
+    box-shadow: 0 10px 20px white,
+                0 -10px 10px purple;
   }
 </style>
 
