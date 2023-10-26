4d96ec68142539d0d41afd994481ddec8788252f
Shawn2516 <shawnmusick@gmail.com>
2023-10-26 16:14:40 +0300

    Updated Nav + Body
    Added some text to navigation.
    Added dark background.
    Added changes file.

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -37,4 +37,7 @@ const count = ref(0)
 .read-the-docs {
   color: #888;
 }
+text {
+  color: white;
+}
 </style>
--- vite-project/src/components/Navigation.vue
+++ vite-project/src/components/Navigation.vue
@@ -1,14 +1,34 @@
 <template>
     <div class="nav">
-
+      <ul>
+        <li>Home</li>
+        <li>Blog</li>
+        <li>Other Feature</li>
+      </ul>
     </div>
   </template>
 
 
-<style>
+<style scoped>
 .nav {
     background-color: black;
     height: 50px;
     width: 100%;
+
 }
+
+ul {
+    display: flex;
+    justify-content: center;
+    list-style-type: none;
+
+}
+
+li {
+    color: orange;
+    padding: 5px;
+    margin: 5px;  
+    font-weight: bold;
+}
+
 </style>
